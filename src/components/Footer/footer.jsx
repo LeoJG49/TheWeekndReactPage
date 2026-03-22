37bdd243063285fdaa0537747dab4fc1d6b156f7
Leo Dev <leoneljovel49@gmail.com>
2026-03-22 19:09:24 +0100

    Refactor Footer component to use CSS modules
    Replace inline styles and className strings with CSS module classes for better maintainability and separation of concerns.

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import FooterContent from './footerContent'
+import styles from './footer.module.css'
 
 export default function Footer() {
   return (
-    <footer 
-      className='relative h-[800px]'
-      style={{clipPath: 'polygon(0% 0, 100% 0%, 100% 100%, 0 100%)'}}
-    >
-      <div className='fixed h-[800px] w-full bottom-0'>
+    <footer className={styles.footerContainer}>
+      <div className={styles.footerContent}>
         <FooterContent />
       </div>
     </footer>
