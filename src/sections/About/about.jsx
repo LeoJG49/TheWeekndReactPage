6cd3275ce5df85fc924dd0eafd7bef8773b44571
Leo Dev <leoneljovel49@gmail.com>
2026-03-25 18:58:38 +0100

    Refactor About section to use separate components for biography and horizontal content

--- src/sections/About/about.jsx
+++ src/sections/About/about.jsx
@@ -1,12 +1,13 @@
 import styles from './about.module.css'
 import React from 'react'
+import Biography from './AboutContent/biography.jsx'
+import HorizontalContent from './AboutContent/horizontalContent.jsx'
 
 export default function About() {
   return (
     <section className={styles.about}>
-      <p className={styles.aboutText}>
-        The Weeknd, born Abel Makkonen Tesfaye on February 16, 1990, in Toronto, is a renowned singer, songwriter, and record producer, celebrated for his unique voice and genre-blending music across R&B, pop, and hip-hop. Gaining fame from his 2011 mixtapes "House of Balloons," "Thursday," and "Echoes of Silence," he released hit albums like "Beauty Behind the Madness," "Starboy," and "After Hours," featuring popular songs such as "Can&apos;t Feel My Face," "Starboy," and "Blinding Lights." His music often tackles themes of love and excess, earning him multiple awards, including Grammys, over the years.
-      </p>
+      <HorizontalContent />
+      <Biography />
     </section>
   )
 }
