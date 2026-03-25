2c110d4387d6aac270341907240098d69e7c19d5
Leo Dev <leoneljovel49@gmail.com>
2026-03-25 22:47:38 +0100

    Refactor Lenis smooth scroll setup into custom hook
    Extract Lenis initialization and cleanup logic from App component into reusable `useLenis` hook for better code organization and reusability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
 'use client'
+import { useLenis } from "./hooks/useLenis.js"
 import Header from "./components/Header/header"
 import Hero from "./sections/Hero/hero.jsx"
 import About from "./sections/About/about.jsx"
 import Discography from "./sections/Discography/discography.jsx"
 import Tour from "./sections/Tour/tour.jsx"
 import Footer from "./components/Footer/footer.jsx"
-import { useEffect } from "react"
-import Lenis from "lenis"
-import gsap from "gsap"
-import ScrollTrigger from "gsap/ScrollTrigger"
 
-gsap.registerPlugin(ScrollTrigger)
 
 //TODO: Hacer que funcione la animación horizontal de About.
 // Arreglar el bug que hace que no se pueda scrollear en la web con el ratón.
 
 function App() {
-
-  useEffect(() => {
-    const lenis = new Lenis()
-    lenis.on("scroll", ScrollTrigger.update)
-    
-    function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
-    }
-  
-    requestAnimationFrame(raf)
-
-    return () => {
-      lenis.destroy()
-    }
-  }, [])
+  useLenis()
 
   return (
     <>
