0b1ae5b402a354c4b6c1772738d0827f5072ec2f
Leo Dev <leoneljovel49@gmail.com>
2026-03-31 22:33:22 +0200

    Add Discography section component
    Create new Discography section that renders DiscographyCards component

---
+++ src/sections/Discography/Discography.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DiscographyCards from '../../components/DiscographyItems/DiscographyCards.jsx'
+
+export default function Discography() {
+  return (
+    <>
+      <DiscographyCards />
+    </>
+  )
+}
