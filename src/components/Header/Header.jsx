24196a083d5c00e7fa59eb177bdf432648595f06
Leo Dev <leoneljovel49@gmail.com>
2026-04-04 22:53:08 +0200

    Add 'use client' directive to Header component and remove Tour link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+'use client'
 import styles from './Header.module.css'
 
 export default function Header() {
@@ -7,7 +8,6 @@ export default function Header() {
         <a className='hover-underline' href="#"><span>HOME</span></a>
         <a className='hover-underline' href="#"><span>ABOUT</span></a>
         <a className='hover-underline' href="#"><span>DISCOGRAPHY</span></a>
-        <a className='hover-underline' href="#"><span>TOUR</span></a>
       </nav>
     </header>
   )
