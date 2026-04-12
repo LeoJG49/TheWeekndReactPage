89c1057c7200d39a0f88f2874c60f3ec2774a0ce
Leo Dev <leoneljovel49@gmail.com>
2026-04-12 18:52:47 +0200

    Update DiscographyCards component styles and improve text responsiveness

--- src/components/DiscographyItems/DiscographyCards.jsx
+++ src/components/DiscographyItems/DiscographyCards.jsx
@@ -11,24 +11,26 @@ export default function DiscographyCards() {
 
   return (
     <section ref={sectionRef} className={styles.stickyCards}>
-      {CARDS_DATA.map(({ id, zIndex, image, text, alt, count, title }, i) => (
-        <div
-          key={id}
-          ref={(el) => (cardsRef.current[i] = el)}
-          className={styles.card}
-          style={{ zIndex }}
-          id={id}
-        >
-          <div className={styles.col}>
-            <h3>{title}</h3>
-            <p>{text}</p>
-            <h5>{count}</h5>
+      {CARDS_DATA.map(({ id, zIndex, image, text, alt, count, title }, i) => {
+        return(
+          <div
+            key={id}
+            ref={(el) => (cardsRef.current[i] = el)}
+            className={styles.card}
+            style={{ zIndex }}
+            id={id}
+          >
+            <div className={styles.col}>
+              <h3>{title}</h3>
+              <p className={styles.cardText}>{text}</p>
+              <h5>{count}</h5>
+            </div>
+            <div className={styles.col}>
+              <img src={image} alt={alt} loading='lazy'/>
+            </div>
           </div>
-          <div className={styles.col}>
-            <img src={image} alt={alt} loading='lazy'/>
-          </div>
-        </div>
-      ))}
+          )
+      })}
     </section>
   );
 }
